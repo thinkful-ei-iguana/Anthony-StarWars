bdfdb9874f566b2057d2dc6a26cba30ae3e187f4
AnthonyTB <Anthonylol@protonmail.com>
2019-11-22 14:48:35 -0800

    fixed typo x2

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -24,7 +24,7 @@ export default class SearchForm extends React.Component {
           <input
             type="text"
             id="searchTerm"
-            placeholder="Enter your search term (Optional)"
+            placeholder="Enter your search term"
           />
           <button id="submitBtn">search</button>
         </form>
